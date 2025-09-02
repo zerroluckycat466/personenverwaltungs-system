0a52f43d3e95a68b39b29def52d14616ca9f7d55
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-09-03 00:28:46 +0800

    feat: initial commit for frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Personenverwaltungs-System</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
